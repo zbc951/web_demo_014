fd03456575a54c04a3d2b8534642ef050cd0a917
lazio@wataidesign.com <lazio@wataidesign.com>
2023-05-18 16:53:49 +0800

    112-0518-1-L

---
+++ js/js.jsx
@@ -0,0 +1,11 @@
+//marqee
+$(document).on('ready', function() {
+	$(".single-item").slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		arrows: true,
+	})
+})
